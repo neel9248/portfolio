c26da0be2577eb4719a430f5676b4b66fbf9c6d1
Neel Trivedi <neeltrivedi@Neels-MacBook-Pro.local>
2020-07-25 23:17:13 -0400

    deploy react app to gh-pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className= "App">
+      <h1>My portfolio</h1>
+      <div>These humble beginning will soon become a personal website showing 
+        my programming journey and accomplishments
+      </div>
+    </div>
+  );
+}
+
+export default App;
