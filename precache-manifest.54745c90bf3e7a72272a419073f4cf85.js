d0dc7eba3de286001e342c9d3f1d4832feb90692
Neel Trivedi <neeltrivedi@Neels-MacBook-Pro.local>
2020-07-25 23:17:38 -0400

    Updates

---
+++ precache-manifest.54745c90bf3e7a72272a419073f4cf85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e57b956d08c3d375b8209a7c7e6094e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "14e20d506892fd7ce7e4",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e20d506892fd7ce7e4",
+    "url": "/portfolio/static/js/main.be5a3562.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
